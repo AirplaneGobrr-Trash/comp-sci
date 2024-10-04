0cb3ef3f71cd300104c3fa24b61ae8ebc2b84bcc
AirplaneGoBrr <mottlcompound@gmail.com>
2024-10-04 13:32:34 -0700

    Port change

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ const app = express();
 let pictureEncoder = require("./Unit2/Leason3/index")
 app.use(pictureEncoder)
 
-app.listen("3000", ()=>{
-    console.log("Running")
+app.listen("3005", ()=>{
+    console.log("Running on :3005")
 })
